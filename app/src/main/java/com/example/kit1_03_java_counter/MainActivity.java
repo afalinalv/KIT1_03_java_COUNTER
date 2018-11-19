aa7c56c868ee59b34c173d41e549962146de8991
afalinalv <afalinalv@ya.ru>
2018-11-19 18:29:30 +0300

    COUNTER EXAMPLE AS 3.3.0-betta4 Gradle 4.10.1 Androidx + java 1.8 API 28-22

--- app/src/main/java/com/example/kit1_03_java_counter/MainActivity.java
+++ app/src/main/java/com/example/kit1_03_java_counter/MainActivity.java
@@ -3,12 +3,55 @@ package com.example.kit1_03_java_counter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mKitsCounterButton;
+    private Button mDolphinsCounterButton;
+    private TextView helloTextView;
+    public int mCountKits = 0;
+    public int mCountDolphins = 0;
+
+    private static final String KEY_COUNT_Dolphins = "COUNT_DOLPHINS";
+    private static final String KEY_COUNT_Kits = "COUNT_KITS";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        helloTextView = findViewById(R.id.textView);
+        mKitsCounterButton = findViewById(R.id.button_Counter_Kits);
+        mDolphinsCounterButton = findViewById(R.id.button_Сounter_Dolphins);
+        /*mKitsCounterButton.setOnClickListener(new View.OnClickListener() {
+                                                   @Override
+                                                   public void onClick(View view) {
+          helloTextView.setText("Я насчитал " + ++mCount + " Китов");
+                                                   }
+                                               });
+         */
+        mKitsCounterButton.setOnClickListener(v ->
+                helloTextView.setText("Я насчитал " + ++mCountKits + " Китов"));
+        mDolphinsCounterButton.setOnClickListener(v ->
+                helloTextView.setText("Я насчитал " + ++mCountDolphins + " Дельфинов"));
+        if (savedInstanceState != null) {
+            mCountKits = savedInstanceState.getInt(KEY_COUNT_Kits, 0);
+            mCountDolphins = savedInstanceState.getInt(KEY_COUNT_Dolphins, 1);
+            // helloTextView.setText("Я насчитал " + mCountDolphins + " Дельфинов");
+            //helloTextView.setText("Я насчитал " + mCountKits + " Китов");
+        }
+    }
+    public void onClick(View view) {
+
+        helloTextView.setText("Hello AFALINA! ");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_COUNT_Dolphins, mCountDolphins);
+        outState.putInt(KEY_COUNT_Kits, mCountKits);
+
     }
 }
